c072a8361dfa5e179a6c7e38e3ca3870133d983e
Abel-alx-github <abeltibeso323@gmail.com>
2024-03-07 13:34:35 -0800

    full deploy web static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -30,16 +30,16 @@ def do_deploy(archive_path):
         put(archive_path, '/tmp/')
         tar_filename = archive_path.split("/")[-1]
         filename = tar_filename.split(".")[0]
-        run('mkdir -p /data/web_static/releases/{}'.format(filename))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format
+        run('sudo mkdir -p /data/web_static/releases/{}'.format(filename))
+        run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format
             (tar_filename, filename))
-        run('rm /tmp/{}'.format(tar_filename))
-        run('mv /data/web_static/releases/{}/web_static/* \
+        run('sudo rm /tmp/{}'.format(tar_filename))
+        run('sudo mv /data/web_static/releases/{}/web_static/* \
             /data/web_static/releases/{}/'.format(filename, filename))
-        run('rm -rf /data/web_static/releases/{}/web_static'
+        run('sudo rm -rf /data/web_static/releases/{}/web_static'
             .format(filename))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{} /data/web_static/current'
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s /data/web_static/releases/{} /data/web_static/current'
             .format(filename))
         return True
     except Exception as error:

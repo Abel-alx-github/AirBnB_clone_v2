168dae2146d11f64f56abce6bf44cbf76e021212
Abel-alx-github <abeltibeso323@gmail.com>
2024-03-07 12:25:24 -0800

    full deploy web static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -39,7 +39,7 @@ def do_deploy(archive_path):
             /data/web_static/releases/{}/'.format(filename, filename))
         run('rm -rf /data/web_static/releases/{}/web_static'
             .format(filename))
-        run('rm -rf /data/web_static/currnt')
+        run('rm -rf /data/web_static/current')
         run('ln -s /data/web_static/releases/{} /data/web_static/current'
             .format(filename))
         return(True)

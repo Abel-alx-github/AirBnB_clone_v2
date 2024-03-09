44fb388780d4a5a6aeb31ef0a7e999527cf623f5
Abel-alx-github <abeltibeso323@gmail.com>
2024-03-09 10:57:36 -0800

    clean

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -12,13 +12,16 @@ env.hosts = ['18.233.67.128', '100.25.162.166']
 def do_clean(number=0):
     """remove out-of-date archives"""
 
+    ls_files = local("ls -1t versions", capture=True).split("\n")
+
     num = int(number)
-
     if num == 0 or num == 1:
-        num = 2
-    else:
-       num += 1
-
-    local('cd versions ; ls -t | tail -n +{} | sudo xargs rm -rf'.format(num))
-    cmd = 'sudo cd /data/web_static/releases'
-    run('{}; ls -t | tail -n +{} | sudo xargs rm -rf'.format(cmd, num))
+        num = 1
+    for File in ls_files[num:]:
+        local("rm versions/{}".format(File))
+    ls_dir_remote = run("ls -1t /data/web_static/releases").split("\n")
+    for Dir in ls_dir_remote[num:]:
+        if Dir == 'test':
+            continue
+        run("rm -rf /data/web_static/releases/{}"
+            .format(Dir))

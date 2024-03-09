9a6b5333113c652729a8eab7c8b68782b2267031
Abel-alx-github <abeltibeso323@gmail.com>
2024-03-09 08:49:32 -0800

    do_clean

---
+++ 100-clean_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+fabric script (based on the file 3-deploy_web_static.py) that
+deletes old archive with do_clean
+"""
+from fabric.operations import *
+
+
+env.user = "ubuntu"
+env.hosts = ['18.233.67.128', '100.25.162.166']
+
+
+def do_clean(number=0):
+    """remove out-of-date archives"""
+
+    num = int(number)
+
+    if num == 0:
+        num = 2
+    else:
+        num += 1
+
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(num))
+    cmd = 'cd /data/web_static/releases'
+    run('{}; ls -t | tail -n +{} | xargs rm -rf'.format(cmd, num))

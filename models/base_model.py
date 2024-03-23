cd595e8a1c5b6db11e2792536f69141ef480377c
abel-alx-github <abeltibeso323@gmail.com>
2024-03-23 13:42:38 -0700

    task 7

--- models/base_model.py
+++ models/base_model.py
@@ -41,9 +41,9 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        dic = self.__dict__
-
-        return '[{}] ({}) {}'.format(cls, self.id, self.to_dict())
+        dic = self.to_dict()
+        del dic['__class__']
+        return '[{}] ({}) {}'.format(cls, self.id, dic)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""

dd3c7301cee6754178c45ba82e95e23e08cdf2e2
abel-alx-github <abeltibeso323@gmail.com>
2024-03-24 08:21:02 -0700

    task 8..

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -12,6 +12,8 @@ from models.user import User
 from models.amenity import Amenity
 from models.review import Review
 from models.place import Place
+classes = {"State": State, "City": City, "User": User,
+           "Place": Place, "Review": Review, "Amenity": Amenity}
 
 
 class DBStorage:
@@ -32,6 +34,7 @@ class DBStorage:
         if env == 'test':
             Base.metadata.drop_all(self.__engine)
 
+    """
     def all(self, cls=None):
         ''' fetch all obj'''
         objects = {}
@@ -43,6 +46,33 @@ class DBStorage:
                 objects.update({obj.id: obj for obj in
                                self.__session.query(cls).all()})
         return objects
+    """
+    def all(self, cls=None):
+        """return dictionary of instance attributes
+        Args:
+        cls (obj): memory address of class
+        Returns:
+        dictionary of objects
+        """
+        dbobjects = {}
+        if cls:
+            if type(cls) is str and cls in classes:
+                for obj in self.__session.query(classes[cls]).all():
+                    key = str(obj.__class__.__name__) + "." + str(obj.id)
+                    val = obj
+                    dbobjects[key] = val
+            elif cls.__name__ in classes:
+                for obj in self.__session.query(cls).all():
+                    key = str(obj.__class__.__name__) + "." + str(obj.id)
+                    val = obj
+                    dbobjects[key] = val
+        else:
+            for k, v in classes.items():
+                for obj in self.__session.query(v).all():
+                    key = str(v.__name__) + "." + str(obj.id)
+                    val = obj
+                    dbobjects[key] = val
+        return dbobjects
 
     def new(self, obj):
         ''' add new obj'''
